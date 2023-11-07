b584066b2c43d55b5139335b3e1630f75ebf1484
agustinateme <128619619+agustinateme@users.noreply.github.com>
2023-11-06 21:04:25 -0300

    Update views.router.js

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,13 +1,66 @@
-import { Router } from "express";
-import Products from '../dao/dbManagers/products.managers.js';
-import Carts from '../dao/dbManagers/carts.managers.js'
+import express from 'express';
+import {ProductsModel} from '../dao/dbManagers/models/products.models.js';
+import {CartsModel} from '../dao/dbManagers/models/carts.models.js';
 
-const router = Router();
+const router = express.Router();
 
-const productManager = new Products();
-const cartManager = new Carts();
+// Ruta para mostrar los productos con paginación
+router.get('/products', async (req, res) => {
+    let { page = 1, limit = 10 } = req.query;
+    page = parseInt(page, 10);
+    limit = parseInt(limit, 10);
+
+    try {
+        const options = {
+            page: page,
+            limit: limit,
+            lean: true,
+            leanWithId: false
+        };
+
+        const result = await ProductsModel.paginate({}, options);
+
+        res.render('products', {
+            products: result.docs,
+            page: result.page,
+            totalPages: result.totalPages,
+            hasNextPage: result.hasNextPage,
+            hasPrevPage: result.hasPrevPage,
+            prevPage: result.prevPage,
+            nextPage: result.nextPage,
+            limit: result.limit
+        });
+    } catch (error) {
+        res.status(500).render('error', { message: 'Error al cargar la lista de productos.' });
+    }
+});
 
 
+// Ruta para mostrar un carrito específico con sus productos
+router.get('/carts/:cid', async (req, res) => {
+    const cartId = req.params.cid;
+
+    try {
+        const cart = await CartsModel.findById(cartId).populate('products.product');
+
+        if (!cart) {
+            return res.status(404).render('error', { message: 'Carrito no encontrado.' });
+        }
+
+        // Calcular el subtotal de cada producto y añadirlo al objeto de producto
+        const productsWithSubtotals = cart.products.map(item => {
+            return {
+                ...item.toObject(), // Convierte el subdocumento de Mongoose a un objeto simple
+                subtotal: item.quantity * item.product.price // Calcula el subtotal
+            };
+        });
+
+        // Renderizar la plantilla 'cart' con los productos y sus subtotales
+        res.render('carts', { products: productsWithSubtotals });
+    } catch (error) {
+        res.status(500).render('error', { message: 'Error al cargar el carrito.' });
+    }
+});
 
 /*
 router.get('/products-view', async (req, res) => {
